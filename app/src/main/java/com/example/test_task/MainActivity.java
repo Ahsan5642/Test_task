f3d0facc73a4e64471b47311211d5aeaa5df78ad
Ahsan5642 <ahsan_4824@hotmail.com>
2021-06-10 11:08:39 +0500

    Test Task

---
+++ app/src/main/java/com/example/test_task/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.test_task;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationItemView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView navView = findViewById(R.id.bottomNavigationView);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.homefragment, R.id.productfragment, R.id.cartfragment)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.fragment);
+
+        NavigationUI.setupWithNavController(navView, navController);
+
+
+
+
+
+    }
+}
